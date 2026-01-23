ffadcda454c062485c53db23d71a86e5a0bbb764
Marta Binkowska <109827646+maarbin@users.noreply.github.com>
2026-01-23 21:17:54 +0100

    Add generator of fake legacy data

---
+++ src/utils/data_generator.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from faker import Faker
+import random
+import sqlalchemy
+
+fake = Faker()
+
+RECORDS_COUNT = 1_000
+
+
+def generate_dirty_date():
+    # Generate random date format with 1% chance of None
+    date_format = random.choice(["%Y-%m-%d", "%d/%m/%Y", "%d.%m.%Y"])
+
+    if random.random() < 0.01:
+        return None
+
+    return fake.date(pattern=date_format)
+
+
+def generate_dirty_phone():
+    # Generate phone number with 5% chance of None
+    if random.random() < 0.05:
+        return None
+
+    return fake.basic_phone_number()
+
+
+generated_data = []
+emails_cache = []
+
+for x in range(RECORDS_COUNT):
+    # Create email duplicates
+    if (len(emails_cache) > 0) and (random.random() < 0.1):
+        email = random.choice(emails_cache)
+    else:
+        email = fake.email()
+        emails_cache.append(email)
+
+    record = {
+        "client_id": x + 1000,
+        "full_name": fake.name(),
+        "email": email,
+        "phone": generate_dirty_phone(),
+        "date": generate_dirty_date(),
+        "account_status": random.choice([0, 1, "Y", "N", "Active"]),
+    }
+
+    generated_data.append(record)
+
+legacy_data = pd.DataFrame(generated_data)
+print(legacy_data.head())
